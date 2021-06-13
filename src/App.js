69b9a595d155d99ceeef3ee8cf6f082bb8b1eeaf
Nikhil Gupta <nikhigu@adobe.com>
2021-06-13 20:33:10 +0530

    Fixes Web3

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
 import React, { useState } from 'react';
-import { Navbar, Container, Row, Col } from 'react-bootstrap';
+import { Navbar, Container, Row, Col, Button } from 'react-bootstrap';
 import Web3 from 'web3';
 import NGTokenABI from './abis/NGToken';
 
-const web3 = new Web3(Web3.givenProvider);
-const contractAddress = '0xc05aB6B5d736B2ee93bBCb70c8dC7dA4188b798A';
-const NGToken = new web3.eth.Contract(NGTokenABI, contractAddress);
+let web3;
+let NGToken;
 
 function App() {
   const [address, setAddress] = useState('0x0');
   const [etherBalance, setEtherBalance] = useState(0);
   const [tokens, setTokens] = useState(0);
 
+  const loadEtherBalance = async (addr) => {
+    const balance = await web3.eth.getBalance(addr);
+    setEtherBalance(balance);
+  };
+
+  const loadTokens = async (addr) => {
+    const tokenBalance = await NGToken.methods.balanceOf(addr).call();
+    setTokens(tokenBalance);
+  };
+
   const loadUser = async () => {
     if (window.ethereum === undefined) return;
     const accounts = await web3.eth.getAccounts();
     const account = accounts[0];
     setAddress(account);
+    loadEtherBalance(account);
+    loadTokens(account);
   };
+  
+  const loadWeb3 = async () => {
+    web3 = new Web3(Web3.givenProvider);
+    const contractAddress = '0xc05aB6B5d736B2ee93bBCb70c8dC7dA4188b798A';
+    NGToken = new web3.eth.Contract(NGTokenABI, contractAddress);
 
-  loadUser();
-
-  const loadEtherBalance = async () => {
-    if (address === '0x0') return;
-    const balance = await web3.eth.getBalance(address);
-    setEtherBalance(balance);
+      loadUser();
   };
-
-  loadEtherBalance();
-
-  const loadTokens = async () => {
-    if (address === '0x0') return;
-    const tokenBalance = await NGToken.methods.balanceOf(address).call();
-    setTokens(tokenBalance);
-  };
-
-  loadTokens();
 
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">NG Tokens</Navbar.Brand>
         <Navbar.Collapse className="justify-content-end">
+          {address !== '0x0' 
+          ?
           <Navbar.Text>
-             {address}
+            {address}
           </Navbar.Text>
+          : 
+          <Button type="submit" onClick={loadWeb3}>Submit</Button>
+          }
         </Navbar.Collapse>
       </Navbar>
       <br />
